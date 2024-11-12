15c3e4abac8b8182bc47b1c6abe068bb46b848a6
Amin <amin.benazzouz@edu.univ-eiffel.fr>
2024-11-12 12:16:04 +0100

    mise en page de la deuxième section et finalisation du code javascript pour la première section

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@
     const avatarenfant = document.getElementById('avatar_enfant');
     const avataradulte = document.getElementById('avatar_adulte');
     const avatarvieille = document.getElementById('avatar_vieille');
+    const avatar = document.getElementById('avatar');
 
     // Mise en place des const age pour sélectionné les li 
     const age1 = document.getElementById('age1');
@@ -20,119 +21,70 @@
 
 
     // Code javascript pour afficher l'avatar et le texte pour la catégorie enfant
-
-        // Code pour l'affichage au survol de la souris
-
-            // Code pour l'affichage de l'avatar
             age1.addEventListener('mouseenter', () => {
-                avatarenfant.classList.add('visible'); 
-                avatarenfant.classList.remove('hidden'); 
-            });
+                avatar.classList.add('hidden')
+                avatar.classList.remove('visible')
+                avataradulte.classList.add('hidden')
+                avataradulte.classList.remove('visible')
+                avatarvieille.classList.add('hidden')
+                avatarvieille.classList.remove('visible')
+                texte.classList.add('hidden')
+                texte.classList.remove('visible')
+                info2.classList.add('hidden')
+                info2.classList.remove('visible')
+                info3.classList.add('hidden')
+                info3.classList.remove('visible')
 
-            age1.addEventListener('mouseleave', () => {
-                avatarenfant.classList.add('hidden'); 
-                avatarenfant.classList.remove('visible'); 
-            });
 
-            // Code pour l'afficahge des informations textuelles
+                avatarenfant.classList.remove('hidden')
+                avatarenfant.classList.add('visible')
+                info1.classList.add('visible')
+                info1.classList.remove('hidden')
 
-            age1.addEventListener('mouseenter', () => {
-                texte.classList.add('hidden');
-                texte.classList.remove('visible');
             })
-
-            age1.addEventListener('mouseleave', () => {
-                texte.classList.add('visible');
-                texte.classList.remove('hidden');
-            })
-
-            age1.addEventListener('mouseenter', () =>{
-                info1.classList.add('visible');
-                info1.classList.remove('hidden');
-            })
-
-            age1.addEventListener('mouseleave', () =>{
-                info1.classList.add('hidden');
-                info1.classList.remove('visible');
-            })
-
-        // Code pour l'affichage définitif au click
-
 
     // Code javascript pour afficher l'avatar et le texte pour la catégorie adulte
-
-        // Code pour le survol de la souris 
-
-            // Code pour l'affichage de l'avatar
             age2.addEventListener('mouseenter', () => {
-                avataradulte.classList.add('hidden');
-                avataradulte.classList.add('visible');
+                avatarenfant.classList.add('hidden')
+                avatarenfant.classList.remove('visible')
+                avatarvieille.classList.add('hidden')
+                avatarvieille.classList.remove('visible')
+                avatar.classList.add('hidden')
+                avatar.classList.remove('visible')
+
+                info1.classList.add('hidden')
+                info1.classList.remove('visible')
+                info3.classList.add('hidden')
+                info3.classList.remove('visible')
+                texte.classList.add('hidden')
+                texte.classList.remove('visible')
+
+                avataradulte.classList.add('visible')
+                avataradulte.classList.remove('hidden')
+                info2.classList.add('visible')
+                info2.classList.remove('hidden')
+
             })
 
-            age2.addEventListener('mouseleave', () => {
-                avataradulte.classList.add('hidden'); 
-                avataradulte.classList.remove('visible'); 
-            });
 
-                // Code pour les informations textuelles
-
-                age2.addEventListener('mouseenter', () => {
-                    texte.classList.add('hidden');
-                    texte.classList.remove('visible');
-                })
-
-                age2.addEventListener('mouseleave', () => {
-                    texte.classList.add('visible');
-                    texte.classList.remove('hidden');
-                })
-
-                age2.addEventListener('mouseenter', () =>{
-                    info2.classList.add('visible');
-                    info2.classList.remove('hidden');
-                })
-
-                age2.addEventListener('mouseleave', () =>{
-                    info2.classList.add('hidden');
-                    info2.classList.remove('visible');
-                })
-
-            // Code pour l'afichage définitf au click
 
     // Code javascript pour afficher l'avatar et le texte pour la catégorie senior
-
-        // Code pour le survol de la souris 
-
-            // Code pour l'avatar
             age3.addEventListener('mouseenter', () => {
-                avatarvieille.classList.add('visible'); 
-                avatarvieille.classList.remove('hidden'); 
-            });
+                texte.classList.add('hidden')
+                texte.classList.remove('visible')
+                info1.classList.add('hidden')
+                info1.classList.remove('visible')
+                info2.classList.add('hidden')
+                info2.classList.remove('visible')
+                avatar.classList.add('hidden')
+                avatar.classList.remove('visible')
+                avataradulte.classList.add('hidden')
+                avataradulte.classList.remove('visible')
+                avatarenfant.classList.add('hidden')
+                avatarenfant.classList.remove('visible')
 
-            age3.addEventListener('mouseleave', () => {
-                avatarvieille.classList.add('hidden'); 
-                avatarvieille.classList.remove('visible'); 
-            });
-
-            //  code pour les textes
-
-            age3.addEventListener('mouseenter', () => {
-                texte.classList.add('hidden');
-                texte.classList.remove('visible');
+                avatarvieille.classList.add('visible')
+                avatarvieille.classList.remove('hidden')
+                info3.classList.add('visible')
+                info3.classList.remove('hidden')
             })
-
-            age3.addEventListener('mouseleave', () => {
-                texte.classList.add('visible');
-                texte.classList.remove('hidden');
-            })
-
-            age3.addEventListener('mouseenter', () =>{
-                info3.classList.add('visible');
-                info3.classList.remove('hidden');
-            })
-
-            age3.addEventListener('mouseleave', () =>{
-                info3.classList.add('hidden');
-                info3.classList.remove('visible');
-            })
-
-        // Code pour l'affichage définitf au click
