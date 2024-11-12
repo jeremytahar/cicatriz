6097769575afa9b3bfaeee361f8660ea602b5aa9
Amin <amin.benazzouz@edu.univ-eiffel.fr>
2024-11-12 15:04:45 +0100

    début du code javascript pour le dernier graphique et début de la mise en page de la section 3

--- script/script.js
+++ script/script.js
@@ -40,7 +40,26 @@
                 avatarenfant.classList.add('visible')
                 info1.classList.add('visible')
                 info1.classList.remove('hidden')
+            })
 
+            age1.addEventListener('click', function (){
+                texte.classList.add('visible')
+                texte.classList.remove('hidden')
+                avatar.classList.add('visible')
+                avatar.classList.remove('hidden')
+
+                info1.classList.add('hidden')
+                info1.classList.remove('visible')
+                info2.classList.add('hidden')
+                info2.classList.remove('visible')
+                info3.classList.add('hidden')
+                info3.classList.remove('visible')
+                avatarenfant.classList.add('hidden')
+                avatarenfant.classList.remove('visible')
+                avataradulte.classList.add('hidden')
+                avataradulte.classList.remove('visible')
+                avatarvieille.classList.add('hidden')
+                avatarvieille.classList.remove('visible')
             })
 
     // Code javascript pour afficher l'avatar et le texte pour la catégorie adulte
@@ -66,6 +85,25 @@
 
             })
 
+            age2.addEventListener('click', function (){
+                texte.classList.add('visible')
+                texte.classList.remove('hidden')
+                avatar.classList.add('visible')
+                avatar.classList.remove('hidden')
+
+                info1.classList.add('hidden')
+                info1.classList.remove('visible')
+                info2.classList.add('hidden')
+                info2.classList.remove('visible')
+                info3.classList.add('hidden')
+                info3.classList.remove('visible')
+                avatarenfant.classList.add('hidden')
+                avatarenfant.classList.remove('visible')
+                avataradulte.classList.add('hidden')
+                avataradulte.classList.remove('visible')
+                avatarvieille.classList.add('hidden')
+                avatarvieille.classList.remove('visible')
+            })
 
 
     // Code javascript pour afficher l'avatar et le texte pour la catégorie senior
@@ -88,3 +126,45 @@
                 info3.classList.add('visible')
                 info3.classList.remove('hidden')
             })
+
+            age3.addEventListener('click', function (){
+                texte.classList.add('visible')
+                texte.classList.remove('hidden')
+                avatar.classList.add('visible')
+                avatar.classList.remove('hidden')
+
+                info1.classList.add('hidden')
+                info1.classList.remove('visible')
+                info2.classList.add('hidden')
+                info2.classList.remove('visible')
+                info3.classList.add('hidden')
+                info3.classList.remove('visible')
+                avatarenfant.classList.add('hidden')
+                avatarenfant.classList.remove('visible')
+                avataradulte.classList.add('hidden')
+                avataradulte.classList.remove('visible')
+                avatarvieille.classList.add('hidden')
+                avatarvieille.classList.remove('visible')
+            })
+
+
+// ========== GRAPHIQUE CONDAMNATION ==========
+
+let ctx = document.getElementById('graph3').getContext('2d');
+
+// spécification des datas 
+
+let data = {
+    labels: ['2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022',],
+    datasets: [{
+        labels: 'Nombre de condamnation',
+        data: [1.024, 1.026, 1.005, 978, 1.088, 806, 1.413, 7.500],
+        backgroundColor: '#4D2A7B',
+        borderColor: '#FFFFFF',
+        borderWidth: 1
+    }]
+}
+
+let graph3 = new graph
+
+console.log(data)
