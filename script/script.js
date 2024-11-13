89204be43752f011b06ade556d1b4b24247d4c6d
Amin <amin.benazzouz@edu.univ-eiffel.fr>
2024-11-13 08:28:02 +0100

    début de la création du dernier graphique

--- script/script.js
+++ script/script.js
@@ -40,3 +40,75 @@ age1.addEventListener('mouseenter', () => displayCategory(avatarenfant, info1));
 age2.addEventListener('mouseenter', () => displayCategory(avataradulte, info2));
 age3.addEventListener('mouseenter', () => displayCategory(avatarvieille, info3));
 
+// ========== GRAPHIQUE CONDAMNATION ==========
+
+let ctx = document.getElementById('graph3').getContext('2d');
+
+// spécification des datas 
+
+let data = {
+    labels: ['2015', '2016', '2017', '2018', '2019', '2020', '2021'],
+    datasets: [
+        {
+        label: 'Nombre de condamnations', // Correction ici
+        data: [1024, 1026, 1005, 978, 1088, 806, 1413, 7500],
+        data2: [512, 550, 400, 300, 700, 400, 780, 1000],
+        backgroundColor: '#FFFFFF',
+        borderColor: '#AF94E0',
+        borderWidth: 3
+        },
+        {
+            label: 'Condamnation classé sans suite',
+            data: [800, 850, 780, 900, 1200, 1100, 1300],
+            backgroundColor: 'rgb(255, 255, 255)',
+            borderColor: 'rgb(255, 255, 255)',
+            borderWidth: 3
+        }
+]};
+
+let graph3 = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        family: 'Alatsi',
+                        weight: 'bold'
+                    },
+                    color: '#FFF'
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    font: {
+                        family: 'Bebas', 
+                        weight: 'normal'
+                    },
+                    color: '#FFF' 
+                }
+            },
+            y: {
+                beginAtZero: true,
+                max: 2000,
+                ticks: {
+                    font: {
+                        family: 'Bebas', 
+                        weight: 'normal'
+                    },
+                    color: '#FFF' 
+                }
+            }
+        }
+    }
+});
+
+
+
+
+
+console.log(data)
